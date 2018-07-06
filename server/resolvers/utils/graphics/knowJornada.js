a9bf0b4e71c8ce23cf3cd516a3f0eec57c74b012
MontoyaAndres <andresmontoyfcb@gmail.com>
2018-07-06 15:17:58 -0500

    Fixing bug on knowJornada file

--- server/resolvers/utils/graphics/knowJornada.js
+++ server/resolvers/utils/graphics/knowJornada.js
@@ -1,17 +1,17 @@
 export default function knowJornada(values) {
   const DISTANCIA = values.filter(value => value.Jornada === 'DISTANCIA').length;
-  const MAÑANA = values.filter(value => value.Jornada === 'MAÑANA').length;
+  const TOMORROW = values.filter(value => value.Jornada === 'MAÑANA').length;
   const NOCHE = values.filter(value => value.Jornada === 'NOCHE').length;
   const NO_JORNADA = values.filter(value => value.Jornada === 'NO JORNADA').length;
 
-  const SUM_VALUES = DISTANCIA + MAÑANA + NOCHE + NO_JORNADA;
+  const SUM_VALUES = DISTANCIA + TOMORROW + NOCHE + NO_JORNADA;
 
-  if (DISTANCIA > 0 || MAÑANA > 0 || NOCHE > 0 || NO_JORNADA > 0) {
+  if (DISTANCIA > 0 || TOMORROW > 0 || NOCHE > 0 || NO_JORNADA > 0) {
     return {
       ok: true,
       values: {
         DISTANCIA: Math.round((DISTANCIA / SUM_VALUES) * 100),
-        MAÑANA: Math.round((MAÑANA / SUM_VALUES) * 100),
+        MAÑANA: Math.round((TOMORROW / SUM_VALUES) * 100),
         NOCHE: Math.round((NOCHE / SUM_VALUES) * 100),
         NO_JORNADA: Math.round((NO_JORNADA / SUM_VALUES) * 100)
       }
