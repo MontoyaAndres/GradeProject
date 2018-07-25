5b2a3d8ea54f8f4cb37c869a7c368cd85a537b7b
MontoyaAndres <andresmontoyfcb@gmail.com>
2018-07-25 15:18:21 -0500

    Remove delete cache from logout

--- client/src/components/Global/Menu/Logout.jsx
+++ client/src/components/Global/Menu/Logout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ApolloConsumer } from 'react-apollo';
 import { Redirect } from 'react-router-dom';
-import { unregister } from '../../../registerServiceWorker';
 
 export default () => (
   <ApolloConsumer>
@@ -9,7 +8,6 @@ export default () => (
       client.resetStore();
       localStorage.removeItem('token');
       localStorage.removeItem('refreshToken');
-      unregister();
       return <Redirect to="/login" />;
     }}
   </ApolloConsumer>
